9020f5aa9b78f520b43c0ac5ac3e56000d925896
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-04 21:27:18 +0200

    console example code
    added example code in console to show copy function

--- FileManagmChall/Program.cs
+++ FileManagmChall/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using FileManagmChall;
+
+string source = @"C:\Temp\Source";
+string destination = @"C:\Temp\Destination";
+
+
+FileProcessor.CopyFiles(source, destination, true, true);
+
+Console.WriteLine("File Copy Complete");
